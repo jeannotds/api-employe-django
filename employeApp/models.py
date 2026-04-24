6fa744fd9b3f0493b3f61888fa857c3eaf107ea3
Jeannot Diambu <diambujeannot@gmail.com>
2026-04-24 16:06:44 +0100

    create migration

--- employeApp/models.py
+++ employeApp/models.py
@@ -1,3 +1,13 @@
+from enum import unique
 from django.db import models
 
 # Create your models here.
+class  Employe(models.Model):
+  nom= models.CharField(max_length=100)
+  poste= models.CharField(max_length=100)
+  salaire= models.DecimalField(max_digits=10, decimal_places=2)
+  email= models.EmailField(unique=True)
+  data_embauche= models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+  return self.nom
