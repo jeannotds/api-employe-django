b20d12bfe473eb71ab3df864042f2b56a3c146a2
Jeannot Diambu <diambujeannot@gmail.com>
2026-04-24 17:00:12 +0100

    create api

--- employeApp/views.py
+++ employeApp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from employeApp.serializers import EmployeSerializer
+from .models import Employe
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def employe_list(request):
+  if request.method == 'GET':
+    employes = Employe.objects.all();
+    serializer = EmployeSerializer(employes, many=True)
+    return Response(serializer.data);
+
+  if request.method == 'POST':
+    serializer = EmployeSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors)
